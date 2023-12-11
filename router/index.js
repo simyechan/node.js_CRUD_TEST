c8b8fb1fb54d40f4ff4b30665b66c606548bfd8a
sim <simyechan87@gmail.com>
2023-12-11 17:30:17 +0900

    crud 완성

--- router/index.js
+++ router/index.js
@@ -1,34 +1,108 @@
 const express = require("express");
+const multer = require("multer");
 const cors = require("cors");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const db = require("./db/db.js");
+
 const app = express();
-const whitelist = ["http://localhost"];
+const conn = db.init();
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, callback) {
+            console.log(file),
+            fs.existsSync("./uploads/") ||
+            fs.mkdirSync("./uploads/", { recursive: !0 }),
+            callback(null, "./uploads/");
+        },
+        filename: function (req, file, callback) {
+            callback(null, file.originalname);
+        },
+    }),
+});
 
 app.set("post", process.env.PORT || 3030);
 app.set("host", process.env.HOST || "0.0.0.0");
 
-app.use(
-    cors({
-        origin(req, res) {
-            console.log("접속된 주소: " + req),
-            -1 == whitelist.indexOf(req) && req
-            ? res(Error("허가되지 않은 주소입니다."))
-            : res(null, !0);
-        },
-        credentials: !0,
-        optionsSuccessStatus: 200,
-    })
-);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.all("/*", function (req, res, next){
-    let ip = req.header.origin;
-    (-1 == whitelist.indexOf(ip) && ip) ||
-    (res.header("Access-Control-Allow-Origin", ip),
-    res.header("Access-Control-Allow-Headers", "X-Requested-With"),
-    next());
+// 게시글 목록 보기
+app.get("/view", function (req, res) {
+    var sql = "select * from board";
+    conn.query(sql, function (err, result) {
+        if (err) console.log("query is not excuted: " + err);
+        else res.send(result);
+    });
 });
 
-app.get("/", function (req, res){
-    res.send("접속된 아이피: " + req.ip);
+// 게시글 작성
+app.post("/insert", upload.single("img"), function (req, res) {
+    var body = req.body;
+    var sql = "SELECT count(*)+1 as bnum FROM board ";
+    conn.query(sql, function (err, result) {
+        if (err) console.log("query is not excuted: " + err);
+        else {
+            var sql = "insert into board (bnum,id,title,content,writedate) values(?,?,?,?,NOW())";
+            var params = [result[0].bnum, body.id, body.title, body.content];
+            conn.query(sql, params, function (err) {
+                if (err) console.log("query is not excuted: " + err);
+                else if (req.file != null) {
+                    var sql = "insert into file (bnum,savefile,filetype,writedate) values (?,?,?,now())";
+                    var params = [body.bnum, req.file.originalname, req.file.mimetype];
+                    conn.query(sql, params, function (err) {
+                        if (err) console.log("query is not excuted: " + err);
+                        else res.sendStatus(200);
+                    });
+                } res.sendStatus(200);
+            });
+        }
+    });
+});
+
+// 게시글 보기
+app.get("/read/:bnum", function (req, res) {
+    var sql = "select * from board where bnum=" + req.params.bnum;
+    conn.query(sql, function (err, result) {
+        if (err) console.log("query is not excuted: " + err);
+        else res.send(result);
+    });
+});
+
+// 게시글 수정
+app.post("/update/:bnum", function (req, res) {
+    var body = req.body;
+    var sql = "update board set id=?, title=?, content=? where bnum=" + req.params.bnum;
+    var params = [body.id, body.title, body.content];
+    conn.query(sql, params, function(err) {
+        if (err) console.log("query is not excuted: " + err);
+        else res.sendStatus(200);
+    });
+});
+
+// 게시글 삭제
+app.get("/delete/:bnum", function (req, res) {
+    var sql = "delete from board where bnum=" + req.params.bnum;
+    conn.query(sql, function (err) {
+        if (err) console.log("query is not excuted: " + err);
+        else res.sendStatus(200);
+    });
+});
+
+// 이미지 파일 불러오기
+app.get("/img/:bnum", function (req, res) {
+    var sql = "select * from file where bnum=" + req.params.bnum;
+    conn.query(sql, function (err, result) {
+        if (err) console.log("query is not excuted: " + err);
+        else if (result.length != 0) {
+            fs.readFile("uploads/" + result[0].savefile, function (err, data) {
+                res.writeHead(200, { "Context-Type": "text/html" });
+                res.end(data);
+            });
+        } else res.sendStatus(200);
+    });
 });
 
 app.listen(app.get("port"), app.get("host"), () => 
